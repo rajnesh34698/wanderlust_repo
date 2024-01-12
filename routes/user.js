4ea23fac0043866b3158e5d2115a24b79c8c0709
Rajnesh <rajnesh34698@gmail.com>
2024-01-12 19:59:26 +0530

    signup user post l-57.10

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,25 @@
 const express=require("express");
 const router=express.Router();
-
+const User=require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
 
+
+router.post("/signup",wrapAsync(async(req,res,next)=>{
+   try {
+    let{username,email,password}=req.body;
+    const newUser=new User({email,username});
+    const registeredUser=await User.register(newUser,password);
+    //console.log(registeredUser);
+    req.flash("success","New User registered successfully");
+    res.redirect("/listings");
+   } catch (err) {
+    req.flash("error",e.message);
+    res.redirect("/signup");
+   };
+}));
 
 module.exports=router;
