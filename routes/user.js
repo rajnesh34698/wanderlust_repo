9cb3ca8be3e92e5e8773103c2fbf31038b3d37c5
Rajnesh <rajnesh34698@gmail.com>
2024-01-12 23:52:26 +0530

    login user l-57.11

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport=require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -21,5 +22,15 @@ router.post("/signup",wrapAsync(async(req,res,next)=>{
     res.redirect("/signup");
    };
 }));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
+    //let{username,password}=req.body;
+    req.flash("success","Welcome back to Wanderlust");
+    res.redirect("/listings");
+});
 
 module.exports=router;
