fc3a5ff982010efdd0153a525114457257207ae7
Rajnesh <rajnesh34698@gmail.com>
2024-01-17 00:53:29 +0530

    logout user 58.2 done

--- routes/user.js
+++ routes/user.js
@@ -33,4 +33,14 @@ router.post("/login",passport.authenticate("local",{failureRedirect:"/login",fai
     res.redirect("/listings");
 });
 
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+        return next(err);
+        }
+        req.flash("success","you are logged out!");
+        res.redirect("/listings");
+    });
+});
+
 module.exports=router;
