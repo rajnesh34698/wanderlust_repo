665035a3f262dd05652b7f03c41e7d38cb1f84fa
Rajnesh <rajnesh34698@gmail.com>
2024-01-20 11:24:59 +0530

    L- 59.4 done we use router.route concept here

--- routes/listing.js
+++ routes/listing.js
@@ -9,21 +9,46 @@ const Listing=require("../models/listing.js");
 const listingController=require("../controllers/listings.js");
 
 
+//New Route
+router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+
+//router.route use karenge same route par jane wali get aur post request ke liye
+router
+.route("/")
 //index route
-router.get("/",wrapAsync(listingController.index));
-   //New Route
-   router.get("/new",isLoggedIn,listingController.renderNewForm);
+.get(wrapAsync(listingController.index))
+//Create route
+.post(isLoggedIn,validateListing,wrapAsync(listingController.createListing));
+
+router
+.route("/:id")
+//specific listing show route
+.get(wrapAsync(listingController.showListing))
+//update route
+.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+//delete route
+.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+
+
+
+
+
+
+//index route
+//router.get("/",wrapAsync(listingController.index));
+   
    //specific listing show route
-   router.get("/:id",wrapAsync(listingController.showListing));
+ //  router.get("/:id",wrapAsync(listingController.showListing));
    
    //Create Route
-   router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));
+//   router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));
    //Edit Route
    router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
    //update route
-   router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
+//   router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
    //delete route
-   router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+//   router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
   
 
    module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,27 @@ const { saveRedirectUrl }=require("../middleware.js");
 const userController=require("../controllers/users.js");
 
 
-router.get("/signup",userController.renderSignupForm);
+
+router
+.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup));
+
+router
+.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.login);
 
 
-router.post("/signup",wrapAsync(userController.signup));
 
-router.get("/login",userController.renderLoginForm);
 
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.login);
+// router.get("/signup",userController.renderSignupForm);
+
+// router.post("/signup",wrapAsync(userController.signup));
+
+// router.get("/login",userController.renderLoginForm);
+
+// router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.login);
 
 router.get("/logout",(req,res,next)=>{
     req.logout((err)=>{
@@ -24,6 +37,6 @@ router.get("/logout",(req,res,next)=>{
         req.flash("success","you are logged out!");
         res.redirect("/listings");
     });
-});
+ });
 
 module.exports=router;
