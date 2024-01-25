af7103f3b6c2c1b1d153f651bd7fadced7557c4d
Rajnesh <rajnesh34698@gmail.com>
2024-01-26 01:34:48 +0530

    L-60.2 done

--- controllers/listings.js
+++ controllers/listings.js
@@ -58,7 +58,9 @@ module.exports.renderEditForm=async(req,res,next)=>{
      req.flash("error","Listing you requested for does not exist");
      res.redirect("/listings");
     }
-    res.render("listings/edit.ejs",{listing});
+    let originalImageUrl=listing.image.url;
+    let imageUrl= originalImageUrl.replace("/upload","/upload/h_300,w_250")
+    res.render("listings/edit.ejs",{listing,imageUrl});
   }
 
   module.exports.updateListing=async(req,res,next)=>{
