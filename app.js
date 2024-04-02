239d1c8faafbff546706c9e8628b30fa786c0a75
Rajnesh <rajnesh34698@gmail.com>
2024-04-02 12:58:54 +0530

    once again

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.get("/about",(req,res)=>{
 });
 
 app.all("*",(req,res,next)=>{
-  res.render("listings/index.ejs");
+  res.redirect("/listings");
   //next(new ExpressError(404,"Page not found"));
 });
 
